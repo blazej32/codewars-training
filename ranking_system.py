0bac70df715100a1b7c85ecb892b863c1c90ff8a
blazej35 <blazej.klepacki1@gmail.com>
2023-12-09 00:59:32 +0100

    Final solution

--- ranking_system.py
+++ ranking_system.py
@@ -14,16 +14,27 @@ class User:
         self.progress = 0
 
     def inc_progress(self, activity_rank):
+        possible_ranks = [-8, -7, -6, -5, -4, -3, -2,
+                          -1, 1, 2, 3, 4, 5, 6, 7, 8]
+        if activity_rank not in possible_ranks:
+            raise ValueError
         rank_difference = activity_rank - self.rank
+        gained_points = 0
         if activity_rank * self.rank < 0:
-            rank_difference -= 1
+            if rank_difference > 0:
+                rank_difference -= 1
+            elif rank_difference < 0:
+                rank_difference += 1
         if rank_difference == 0:
-            gained_points = 3
+            gained_points += 3
         if rank_difference == -1:
-            gained_points = 1
+            gained_points += 1
         if rank_difference > 0:
-            gained_points = 10 * (rank_difference ** 2)
+            gained_points += 10 * (rank_difference ** 2)
 
         ranks_gained = (self.progress + gained_points) // 100
         self.rank = rank_up(self.rank, ranks_gained)
-        self.progress = (self.progress + gained_points) % 100
+        if self.rank == 8:
+            self.progress = 0
+        else:
+            self.progress = (self.progress + gained_points) % 100

1aae9f5dce40298d4ed85663e38799c3ef6a3c6f
blazej32 <blazej.klepacki1@gmail.com>
2024-03-02 17:30:48 +0100

    finish excercise

--- roman_numerals_helper.py
+++ roman_numerals_helper.py
@@ -1,13 +1,13 @@
-map_to_roman = {0: ['', '', '', ''],
-                1: ['I', 'X', 'C', 'M'],
-                2: ['II', 'XX', 'CC', 'MM'],
-                3: ['III', 'XXX', 'CCC', 'MMM'],
-                4: ['IV', 'XL', 'CD'],
-                5: ['V', 'L', 'D'],
-                6: ['VI', 'LX', 'DC'],
-                7: ['VII', 'LXX', 'DCC'],
-                8: ['VIII', 'LXXX', 'DCCC'],
-                9: ['IX', 'XC', 'CM']}
+map_to_roman = [['', '', '', ''],
+                ['I', 'X', 'C', 'M'],
+                ['II', 'XX', 'CC', 'MM'],
+                ['III', 'XXX', 'CCC', 'MMM'],
+                ['IV', 'XL', 'CD'],
+                ['V', 'L', 'D'],
+                ['VI', 'LX', 'DC'],
+                ['VII', 'LXX', 'DCC'],
+                ['VIII', 'LXXX', 'DCCC'],
+                ['IX', 'XC', 'CM']]
 
 
 def to_roman(val: int) -> str:
@@ -18,8 +18,29 @@ def to_roman(val: int) -> str:
     return reversed_roman[::-1]
 
 
-def from_roman(val: str) -> int:
-    pass
+def from_roman(roman_num: str) -> int:
+    arabic = [0, 0, 0, 0]
+    for row in [4, 9]:
+        for num in map_to_roman[row]:
+            if num in roman_num and not arabic[map_to_roman[row].index(num)]:
+                arabic[map_to_roman[row].index(num)] = row
+                roman_num = roman_num.replace(num, '')
+    for row in [8, 7, 6, 5, 3, 2, 1]:
+        for num in map_to_roman[row]:
+            if num in roman_num and not arabic[map_to_roman[row].index(num)]:
+                arabic[map_to_roman[row].index(num)] = row
+    str_arabic = [str(x) for x in arabic[::-1]]
+    return int(''.join(str_arabic).lstrip('0'))
 
 
-print(to_roman(2345))
+
+'''
+    for num in map_to_roman[4]:
+        if num in roman_num:
+            arabic[map_to_roman[4].index(num)] = 4
+            roman_num.replace(map_to_roman[4].index(num), '')
+    for num in map_to_roman[9]:
+        if num in roman_num:
+            arabic[map_to_roman[9].index(num)] = 9
+            roman_num.replace(map_to_roman[9].index(num), '')
+'''

6b65fe7ed65079696b354420f596e75440a82ded
blazej32 <blazej.klepacki1@gmail.com>
2024-03-04 18:42:46 +0100

    close to the solution

--- last_digit_of_a_huge_number.py
+++ last_digit_of_a_huge_number.py
@@ -16,12 +16,17 @@ def last_digit(lst):
     if len(lst) == 2:
         if lst[1] == 0:
             return 1
-        return power_endings[lst[0]][(lst[1] % len(power_endings[lst[0]])) - 1]
+        base = int((str(lst[0]))[-1])
+        exponent = (lst[1] % len(power_endings[base])) - 1
+        return power_endings[base][exponent]
+    for num in range(len(lst)):
+        lst[num] = int((str(lst[num]))[-1])
     while len(lst) != 1:
         new_lst = lst[:-2]
         new_lst.append(last_digit(lst[-2:]))
-        lst = lst.copy()
-    return lst[0]
+        lst = new_lst.copy()
+    return int((str(lst[0]))[-1])
 
 
-print(last_digit([0, 0, 0]))
+for n in range(100):
+    print(int(str(6**n)[-2:]))
